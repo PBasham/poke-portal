e25487822e5157dffa56e37b372a4626b4fc0238
patrickbasham <PatrickTBasham@gmail.com>
2022-06-14 16:56:28 -0400

    added basic team/new.liquid route

--- controllers/team.js
+++ controllers/team.js
@@ -24,15 +24,16 @@ router.use((req, res, next) => {
 // I-N-D-U-C-E-S
 // index route ('/route') - method=GET
 router.get("/", (req, res) => {
-    Pokemon.find({},{name: 1, id: 1, sprites: 1}).sort("id")
-            .then((pokemon) => {
+//     Pokemon.find({},{name: 1, id: 1, sprites: 1}).sort("id")
+//             .then((pokemon) => {
 
-                    res.render("team/index.liquid", { allPokemon: pokemon })
-            })
-            .catch((error) => {
-                    console.log(error)
-                    res.json({ error })
-            })
+//                     res.render("team/index.liquid", { allPokemon: pokemon })
+//             })
+//             .catch((error) => {
+    //                     console.log(error)
+    //                     res.json({ error })
+    //             })
+                        res.render("team/index.liquid")
 })
 // new route ('/route/new') - method=GET
 router.get("/new", (req, res) => {
@@ -43,7 +44,15 @@ router.get("/new", (req, res) => {
 // update route ('/route/:id') - method=PUT
 
 // create route ('/route') - method=POST
-
+router.post("/", (req, res) => {
+    let pageData = req.body
+    console.log(pageData)
+    
+    Team.create(pageData)
+    .then((newTeam) => {
+        res.redirect("/team")
+    })
+})
 // edit route ('/route/:id/edit') - method=GET
 
 // show route ('/route/:id') - method=GET
